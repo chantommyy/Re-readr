e171a1525be70adda17955eeb5d66d59035e795d
Maxime Sidonio <maximesidonio@gmail.com>
2022-11-30 11:28:53 +0000

    added view and routes

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,27 @@
 class ReviewsController < ApplicationController
-end
 
-def index
-  @book = Book.all
+  def create
+    @review = Review.new(review_params)
+    @review.book = @book
+    @review.save
+    redirect_to book_path(@book)
+  end
+
+  def new
+    @review = Review.new
+  end
+
+  def index
+    @book = Book.all
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:rating, :comment)
+  end
+
+  def show
+    @review = Review.find
+  end
 end
