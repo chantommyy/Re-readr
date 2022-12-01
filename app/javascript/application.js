8b5e7017393c22baf6058b9a7c2855987354d962
Maxime Sidonio <maximesidonio@gmail.com>
2022-12-01 15:32:42 +0000

    navbar

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,12 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import "bootstrap"
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
