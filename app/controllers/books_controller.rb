90cecd8e53f1fdcbe4d927f9b0b07435c1f62cf6
dylan deehan <dylan_deehan@msn.com>
2022-12-01 13:39:33 +0000

    reviews CRUD is now working, dont need the own method, getting links to work next

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -14,35 +14,35 @@ class BooksController < ApplicationController
     @book = Book.new
   end
 
-  def create
-    response = RestClient.get("https://api2.isbndb.com/book/9781566190930", {accept: 'application/json', Authorization: '48828_4ec0d4456ee0bfe47c0b200f5528b2c6'})
-    repos = JSON.parse(response)
-    @books = repos["books"]
-    @book = Book.new(
-      name: book["title"],
-      author: book["authors"],
-      photo: book["image"],
-      genre: book["subjects"][0],
-      user_id: current_user.id
-    )
-    if @book.save
-      redirect_to books_path(@book)
-    else
-      render :new, status: :unrprocessable_entity
-    end
-  end
+  # def create
+  #   response = RestClient.get("https://api2.isbndb.com/book/9781566190930", {accept: 'application/json', Authorization: '48828_4ec0d4456ee0bfe47c0b200f5528b2c6'})
+  #   repos = JSON.parse(response)
+  #   @books = repos["books"]
+  #   @book = Book.new(
+  #     name: book["title"],
+  #     author: book["authors"],
+  #     photo: book["image"],
+  #     genre: book["subjects"][0],
+  #     user_id: current_user.id
+  #   )
+  #   if @book.save
+  #     redirect_to books_path(@book)
+  #   else
+  #     render :new, status: :unrprocessable_entity
+  #   end
+  # end
 
   # def get_book_info(barcode)
 
   # end
 
-    # @book = Book.create(book_params)
+  #   @book = Book.create(book_params)
 
-    # when validations added
-    # if @book.save
-    #   redirect_to book_path(@book)
-    # else
-    #   render :new, status: :unrprocessable_entity
+  #   when validations added
+  #   if @book.save
+  #     redirect_to book_path(@book)
+  #   else
+  #     render :new, status: :unrprocessable_entity
   # end
 
   def edit
