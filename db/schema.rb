a61c84fdcaadf2fbc5c36706155b30736fa98316
Tommy Chan <yinleongch@gmail.com>
2022-12-05 13:14:36 +0000

    swap now has user_id

--- db/schema.rb
+++ db/schema.rb
@@ -51,11 +51,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_02_122844) do
   create_table "swaps", force: :cascade do |t|
     t.bigint "request_id", null: false
     t.bigint "book_id", null: false
+    t.bigint "user_id", null: false
     t.boolean "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["book_id"], name: "index_swaps_on_book_id"
     t.index ["request_id"], name: "index_swaps_on_request_id"
+    t.index ["user_id"], name: "index_swaps_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -80,4 +82,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_02_122844) do
   add_foreign_key "reviews", "users", column: "reviewee_id"
   add_foreign_key "swaps", "books"
   add_foreign_key "swaps", "requests"
+  add_foreign_key "swaps", "users"
 end
