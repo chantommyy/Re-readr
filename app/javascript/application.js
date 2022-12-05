680dced66f44edc1424921bc90043768a5d8fcc2
dylan deehan <dylan_deehan@msn.com>
2022-12-05 16:56:44 +0000

    reviews have been added to profile, profile also has tabs for profile books and reviews

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,11 +4,12 @@ import "./controllers"
 import "bootstrap"
 
 
-function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
+// function myFunction() {
+//   var x = document.getElementById("myTopnav");
+//   if (x.className === "topnav") {
+//     x.className += " responsive";
+//   } else {
+//     x.className = "topnav";
+//   }
+// }
+// function to do tabs
